4d29e9b82c4065b14358f715d7774c59d2eba49f
Daniil211 <id20214@gmail.com>
2023-03-12 15:26:04 +0300

    Добавьте файлы проекта.

---
+++ InsertGuid/Commands/MyCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace InsertGuid
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+            var selection = docView.TextView.Selection.SelectedSpans.FirstOrDefault();
+            if(selection != null)
+            {
+                docView.TextBuffer.Replace(selection, Guid.NewGuid().ToString());
+            }
+        }
+    }
+}
